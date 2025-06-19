beeb234498375d7066c79c063823dc1e21e8e3f5
keelann95 <sephen.mwaniki@student.moringaschool.com>
2025-06-19 10:53:19 +0300

    success in creating post route which sends tasks tto json

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,16 @@
+const { error } = require('console');
 const express = require('express');
 const path = require('path');
 const router = express.Router();
 const fs = require('fs').promises
 
+
+
+// finds the exact location of tasks.json even when running from different file
+const pathName = path.join(__dirname, '../tasks.json') 
 //get tasks
 router.get('/', async(req, res)=>{
     try{
-        const pathName = path.join(__dirname, '../tasks.json')
         const data = await  fs.readFile( pathName, 'utf-8')
         console.log("hii there", data);
         res.send(JSON.parse(data))
@@ -21,11 +25,26 @@ router.get('/', async(req, res)=>{
 router.post('/', async (req, res) =>{
     try{
 
-        const tasks = req.body
+        const newTask = req.body //we grab the body the sender sends from frontend
+        if (!newTask.title) {
+             
+         return res.status(400).send({error : 'task must have a title'})
+        } 
 
-        const pathName = path.join(__dirname, '../tasks.json')
-        const data = await fs.readFile(pathName, 'utf-8')
-        return JSON.parse(data)
+        const data = await fs.readFile(pathName, 'utf-8') // it reads the file as  string
+        return JSON.parse(data) //enables it to parse it to usable array
+    
+        newTask.id = tasks.length ? tasks[tasks.length -1 ].id + 1 : 1
+        newTask.completed = false
+
+        tasks.push(newTask)
+
+        await fs.writeFile(pathName, JSON.stringify(tasks, null, 2))
+        res.status().send(newTask)
+    } catch(err){
+        console.error(err);
+        res.status(500).send({error : 'error in saving the task'})
+        
     }
 })
 

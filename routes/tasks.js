a1f5c761de080063795f939876b6fe276fd1f796
keelann95 <sephen.mwaniki@student.moringaschool.com>
2025-06-19 12:38:20 +0300

    adds get task by id and delete a task

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,12 +8,26 @@ const fs = require('fs').promises
 
 // finds the exact location of tasks.json even when running from different file
 const pathName = path.join(__dirname, '../tasks.json') 
+
+
+
+const readTasks = async () => {
+  const data =  await fs.readFile(pathName, 'utf-8')
+  return JSON.parse(data)
+} 
+
+const writeTasks = async (tasks) => {
+    await fs.writeFile(pathName, JSON.stringify(tasks, null, 2))
+
+}
+
 //get tasks
-router.get('/', async(req, res)=>{
+router.get('/', async (req, res)=>{
     try{
         const data = await  fs.readFile( pathName, 'utf-8')
         console.log("hii there", data);
-        res.send(JSON.parse(data))
+        const tasks = JSON.parse(data);
+        res.send(tasks)
         
     }catch(err){
         console.error(err);
@@ -21,6 +35,29 @@ router.get('/', async(req, res)=>{
     }
 })
 
+
+router.get('/:id', async (req, res)=>{
+    try{
+
+        const id =parseInt( req.params.id)
+        const data = await  fs.readFile( pathName, 'utf-8')
+        const tasks = JSON.parse(data);
+    
+        const aTask = tasks.find(task => task.id === id)
+         if(!aTask){
+           return res.status(404).send({error: `task with id ${id} not found`})
+        }
+
+        //write the new tasks to the file
+
+        
+        res.send(aTask)
+        
+    }catch(err){
+        console.error(err);
+        res.status(500).send({error:"Failed to load tasks"})
+    }
+})
 
 router.post('/', async (req, res) =>{
     try{
@@ -32,20 +69,51 @@ router.post('/', async (req, res) =>{
         } 
 
         const data = await fs.readFile(pathName, 'utf-8') // it reads the file as  string
-        return JSON.parse(data) //enables it to parse it to usable array
-    
-        newTask.id = tasks.length ? tasks[tasks.length -1 ].id + 1 : 1
+        const tasks = JSON.parse(data)
+        newTask.id = tasks.length ? tasks[tasks.length - 1 ].id + 1 : 1
         newTask.completed = false
 
         tasks.push(newTask)
 
         await fs.writeFile(pathName, JSON.stringify(tasks, null, 2))
-        res.status().send(newTask)
+        res.status(201).send(newTask)
     } catch(err){
         console.error(err);
         res.status(500).send({error : 'error in saving the task'})
         
     }
 })
+
+router.delete('/:id', async (req, res) => {
+
+    try {
+        //getting the url id which must be a number
+        const id  = parseInt(req.params.id)
+
+        //read the existing tasks
+        const tasks = await readTasks()
+
+        //filter out the task with the id
+
+        const filteredTasks = tasks.filter( task => task.id !== id)
+
+
+        //if the task is not deleted( length the same), return error
+
+        if(filteredTasks.length === tasks.length){
+           return res.status(404).send({error: 'task not found'})
+        }
+
+        //write the new tasks to the file
+
+        await writeTasks(filteredTasks)
+
+        res.send({message : `Task ${id} deleted successfully.`})
+
+    } catch (err) {
+        console.error(err);
+    res.status(500).send({ error: 'Something went wrong while deleting the task.' });        
+    }
+});
 
 module.exports = router;
